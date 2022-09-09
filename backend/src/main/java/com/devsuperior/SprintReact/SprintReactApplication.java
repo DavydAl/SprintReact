c09d9beca607b792b7f3ce77a67ccb85a624cfd0
Davyd Almeida <almeidadavyd@outlook.com>
2022-09-09 13:18:59 -0300

    Commit da versão com a pasta backend configurada

---
+++ backend/src/main/java/com/devsuperior/SprintReact/SprintReactApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.SprintReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/SprintReact/SprintReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.SprintReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
